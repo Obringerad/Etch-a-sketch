78ee613c13af5454d4fca0c77a7d86fbb7c0be43
Alex Obringer <100440555+Obringerad@users.noreply.github.com>
2024-03-07 14:14:31 -0600

    clarified comments

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// Number of divs you want to create
+// Number of divs to create
 const numberOfDivs = 256;
 
 // Get the grid container element
 const gridContainer = document.getElementById('gridContainer');
 
-// Create and append divs to the grid container
+// Creates and appends divs to the grid container
 for (let i = 0; i < numberOfDivs; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-item');
