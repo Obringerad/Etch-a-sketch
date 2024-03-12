a6f2598f54a1e000f34732807f8ced7a00439686
Alex Obringer <100440555+Obringerad@users.noreply.github.com>
2024-03-12 15:01:55 -0500

    finished etch project guidlines

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-var sizeButton=document.getElementById('sizeButton');
+var sizeButton = document.getElementById('sizeButton');
 var userInput;
 let gridContainer = document.getElementById('gridContainer');
 
 //button fuctions
     sizeButton.addEventListener('click', function(){
-
         userInput = parseInt(window.prompt('How many squares per side? (Up to 100)'));
         createGrid(userInput);
         gridContainer.style.setProperty('--divs-per-side', userInput);
-    })
+});
 
 function createGrid(numberOfDivs){
-gridContainer.innerHTML = ''; //clears grid
+    gridContainer.innerHTML = ''; //clears grid
 
-let totalWidth = 400;
-let squareSize = totalWidth / numberOfDivs;
+    let totalDivs = numberOfDivs * numberOfDivs;
 
-gridContainer.style.width = totalWidth + 'px';
-gridContainer.style.height = totalWidth + 'px';
+    let itemWidth = 100 / numberOfDivs;
+    let itemHeight = 100 / numberOfDivs;
 
-// create divs
-for (let i = 0; i < numberOfDivs; i++) {
+    // create grid-item divs
+    for (let i = 0; i < totalDivs; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-item');
+    div.style.width = `${itemWidth}%`;
+    div.style.height = `${itemHeight}%`;
     div.addEventListener('mouseover', function() {
         div.classList.add('hovered');
     });
     gridContainer.appendChild(div);
-}
+    }
+    gridContainer.style.setProperty('--divs-per-side', userInput);
 }
 
-gridContainer.style.setProperty('--divs-per-side', userInput);
